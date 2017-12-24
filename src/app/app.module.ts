dca69163286ad2446d3d60075c9b303526e1e4ab
Dez Buchanon <zdbuchanon@gmail.com>
2017-12-23 22:19:14 -0600

    animations update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { DataService } from './data.service';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -19,6 +19,8 @@ import { AboutComponent } from './about/about.component';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
+    BrowserAnimationsModule
+    
   ],
   providers: [DataService],
   bootstrap: [AppComponent]
